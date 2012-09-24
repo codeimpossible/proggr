751105bffa41a17988e7d56239ea722864efa3d2
Jared Barboza <jared.m.barboza@gmail.com>
2012-09-24 00:29:34 -0400

    adds some unit tests and roughs out the workers concept

---
+++ src/Proggr/Content/js/unittests.js
@@ -0,0 +1,129 @@
+﻿(function ($, worker) {
+    var id = "proggr";
+    var isIE = (/msie/i.test(navigator.userAgent) && !/opera/i.test(navigator.userAgent));
+    var fx = {
+        shouldBeTrue: function (expr, msg) {
+            if (!expr) {
+                throw { wassuccess: expr, message: msg || null };
+            }
+        },
+
+        shouldBeFalse: function (expr, msg) {
+            if (expr) {
+                throw { wassuccess: !expr, message: msg || null };
+            }
+        },
+
+        shouldBeEqual: function (a, b, msg) {
+            if (a != b) {
+                throw { wassuccess: false, message: msg || null };
+            }
+        },
+
+        shouldBeSame: function (a, b, msg) {
+            if (a !== b) {
+                throw { wassuccess: false, message: msg || null };
+            }
+        },
+
+        shouldThrowError: function (expr, msg) {
+            try {
+                fn();
+                throw { wassuccess: false, message: msg || null };
+            } catch (e) {
+
+            }
+        }
+    };
+
+    var goodStyle = {
+        position: "absolute",
+        top: "0",
+        left: "0",
+        width: "10px",
+        height: "10px",
+        'border-right': "1px solid #A2CD5A",
+        'border-bottom': "1px solid #6E8B3D",
+        'line-height': "20px",
+        background: "#BCEE68",
+        padding: "0",
+        margin: "1px"
+    };
+
+    var badStyle = {
+        position: "absolute",
+        top: "0",
+        left: "0",
+        width: "10px",
+        height: "10px",
+        'border-right': "1px solid #CD0000",
+        'border-bottom': "1px solid #8B0000",
+        'line-height': "20px",
+        background: "#CD3333",
+        padding: "0",
+        margin: "1px"
+    };
+
+    var halp = {
+        setStyle: function (e, styleObj) {
+            var styleAttr = "";
+            for (var key in styleObj) {
+                if (styleObj.hasOwnProperty(key)) {
+                    styleAttr += key + ":" + styleObj[key] + ";";
+                }
+            }
+            halp.attr(e, 'style', styleAttr);
+        },
+        attr: function (e, name, val) {
+            e.setAttribute(name, val);
+        },
+        ev: function (e, name, _fn) {
+            var handler = window.addEventListener || window.attachEvent;
+            var prefix = isIE ? "on" : "";
+            handler(prefix + name, _fn, isIE);
+        }
+    };
+
+    worker.tasks.registerTask("running unit tests", {
+        tests:{
+            "registering a task adds it to the task list": function () {
+                var testWorker = new Worker();
+                testWorker.tasks.registerTask("test task", {});
+
+                this.assertEqual(1, testWorker.tasks.list.length);
+                this.assertEqual("test task", testWorker.tasks.list[0].name);
+            },
+            "next returns the first task from the list": function () {
+                var testWorker = new Worker();
+
+                testWorker.tasks.registerTask("test task", {});
+                testWorker.tasks.registerTask("another test task", {});
+
+                this.assertEqual("test task", testWorker.tasks.next().name);
+            }
+        },
+        work: function (workerHandler) {
+            var testsToRun = [];
+
+            for (var key in this.tests) {
+                if (obj.hasOwnProperty(key)) {
+                    testsToRun.push({ name: key, fn: obj[key] });
+                }
+            }
+
+            workerHandler.progress.maxSteps = testsToRun.length;
+
+            for (var i = -1, l = testsToRun.length; ++i < l;) {
+                var testName = testsToRun[i].name;
+                var result = { wassuccess: true, msg: '' };
+                try {
+                    testsToRun[i].fn.call(fx);
+                } catch (e) {
+                    result = e;
+                }
+
+                workerHandler.progress.stepCompleted(i+1);
+            }
+        }
+    });
+})(jQuery, worker);
---
+++ src/Proggr/Content/js/worker.js
@@ -0,0 +1,67 @@
+﻿var Worker = (function ($, global, undefined) {
+    var $progress = $("#progress");
+    var $history = $('#history');
+
+    var percentage = 0;
+
+    $(window).resize(function () {
+        var max_width = $('window').width();
+        percentage = max_width / 100;
+        // TODO: resize the progress using the new value for percent
+    });
+
+    return function() {
+        return {
+            tasks: {
+                list: [],
+                registerTask: function (name, task) {
+                    this.list.push({ name: name, task: task });
+                },
+                next: function () {
+                    return this.list.shift();
+                }
+            },
+            history: {
+                el: $history,
+                add: function (message) {
+                    this.el.append($("<li />", { text: message }));
+                }
+            },
+            progress: {
+                el: $progress,
+                maxSteps: 0,
+                stepCompleted: function ( step ) {
+                    this.el.width(percentage * step);
+                }
+            },
+            work: function () {
+                var task = this.tasks.next();
+                if (task) {
+                    var what = task.name;
+                    var work = task.work;
+                    var parent = this;
+
+                    // set progress on current task to 0
+                    progress.report(0);
+
+                    // set the current task desc into history
+                    history.append($("<li />", { text: what }));
+
+                    // start the task
+                    work(this);
+
+                    history.append($("<li />", { text: "Sleeping" }));
+                }
+                // when the task is done, wait and start again
+                setTimeout(function () {
+                    parent.work(progress, history);
+                }, 1000);
+            },
+            start: function () {
+                this.work($progress, $history);
+            }
+        };
+    };
+})(jQuery, window);
+
+var worker = new Worker();
