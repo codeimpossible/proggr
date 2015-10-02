0b1081b22d8e8fd5c4f4cdbab71c4c1a2758bc86
Jared Barboza <jared.m.barboza@gmail.com>
2015-10-01 23:49:59 -0400

    worker polls for changes every 30s now

---
+++ Proggr.All/Worker/Controllers/JobPoller.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Worker.Jobs;
+using Worker.Models;
+using Worker.Presenters;
+using Worker.Repositories;
+
+namespace Worker.Controllers
+{
+    public class JobPoller
+    {
+        private readonly int _interval = (int)TimeSpan.FromSeconds(30).TotalMilliseconds;
+        private Thread _thread;
+        private bool _stop;
+
+        private readonly WorkloadPresenter _presenter;
+        private readonly IJobRepository _jobRepository;
+        private readonly WorkerState _worker;
+
+        public JobPoller(WorkloadPresenter presenter, IJobRepository jobRepository, WorkerState worker, int? intervalSeconds = null)
+        {
+            _interval = intervalSeconds ?? _interval;
+
+            _presenter = presenter;
+            _jobRepository = jobRepository;
+            _worker = worker;
+        }
+
+        public void Start()
+        {
+            if (_thread == null)
+            {
+                _thread = new Thread(Run);
+                _thread.Start();
+            }
+            _stop = false;
+        }
+
+        private void Run()
+        {
+            while (!_stop)
+            {
+                // Wait the specified interval before checking again...
+                if (_presenter.CurrentJob == null)
+                {
+                    var desc = _jobRepository.GetNextJob(_worker.Id);
+                    if (desc != null)
+                    {
+                        _worker.CurrentJob = desc.Id;
+
+                        // TODO: change this so that it happens automatically when CurrentJob is set on the workerstate
+                        _presenter.CurrentJob = JobFactory.CreateJob(desc, _worker);
+                    }
+                }
+
+                Thread.Sleep(_interval);
+            }
+
+            try
+            {
+                _thread.Abort();
+            }
+            catch (ThreadAbortException tae)
+            {
+            }
+            finally
+            {
+                _thread = null;
+            }
+        }
+
+        public void Stop()
+        {
+            _stop = true;
+        }
+    }
+}
--- Proggr.All/Worker/Program.cs
+++ Proggr.All/Worker/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Timers;
+using System.Threading;
 using System.Windows.Forms;
 using Worker.Controllers;
 using Worker.Jobs;
@@ -12,9 +13,6 @@ namespace Worker
 {
     static class Program
     {
-        // TODO: refactor this out to another object?
-        private static readonly System.Timers.Timer Timer = new System.Timers.Timer(new TimeSpan(0, 0, 0, 30).TotalMilliseconds);
-
         private static WorkloadPresenter _presenter;
         private static IJobRepository _jobRepository;
         private static WorkerState _worker;
@@ -31,8 +29,9 @@ namespace Worker
             // make sure we're able to do work
             _worker = new WorkerRegistrationController(new WorkerRepository()).EnsureWorkerRegistration();
             _jobRepository = new JobRepository();
-
             _presenter = new WorkloadPresenter(new WorkloadView());
+
+            var poller = new JobPoller(_presenter, _jobRepository, _worker);
 
             if (_worker.CurrentJob != Guid.Empty)
             {
@@ -40,24 +39,15 @@ namespace Worker
                 _presenter.CurrentJob = JobFactory.CreateJob(_jobRepository.GetCurrentJob(_worker.Id), _worker);
             }
 
-            Timer.Elapsed += Tick;
-            Tick(null, null);
+            poller.Start();
 
-            Application.Run((WorkloadView)_presenter.View);
-        }
-
-        private static void Tick(object sender, ElapsedEventArgs e)
-        {
-            if (_presenter.CurrentJob == null)
+            try
             {
-                var desc = _jobRepository.GetNextJob(_worker.Id);
-                if (desc != null)
-                {
-                    _worker.CurrentJob = desc.Id;
-
-                    // TODO: change this so that it happens automatically when CurrentJob is set on the workerstate
-                    _presenter.CurrentJob = JobFactory.CreateJob(desc, _worker);
-                }
+                Application.Run((WorkloadView) _presenter.View);
+            }
+            finally
+            {
+                poller.Stop();
             }
         }
     }
